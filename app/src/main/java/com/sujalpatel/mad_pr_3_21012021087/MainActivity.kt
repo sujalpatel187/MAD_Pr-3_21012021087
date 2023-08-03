aa775881a31dcbe2979882a62a18217beaa6c5d1
Sujal Patel <sujalpatel1882@gmail.com>
2023-08-03 22:31:36 +0530

    Initial commit

---
+++ app/src/main/java/com/sujalpatel/mad_pr_3_21012021087/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sujalpatel.mad_pr_3_21012021087
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
